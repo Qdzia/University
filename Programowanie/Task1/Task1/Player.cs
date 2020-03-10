c55785f1a9ba7c4e1c1cbea4c1c7e215055ae566
Lukasz <qdzia.work@gmail.com>
2020-03-10 16:55:54 +0100

    LAB 10.03.20
    Few changes in Task1 form

---
+++ Programowanie/Task1/Task1/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Player
+    {
+        string fn, ln;
+        int age;
+        double weight;
+
+        public Player(string fn = "imie", string ln = "nazwisko", int age = 0, double weight = 0)
+        {
+            this.fn = fn;
+            this.ln = ln;
+            this.age = age;
+            this.weight = weight;
+        }
+        public Player(Player p)
+        {
+            this.fn = p.fn;
+            this.ln = p.ln;
+            this.age = p.age;
+            this.weight = p.weight;
+        }
+
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ Programowanie/Task1/Task1/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class test
+    {
+
+        void tak() {
+
+            Player player = new Player("aa","aa",12,13.0);
+
+        
+        }
+    }
+}
